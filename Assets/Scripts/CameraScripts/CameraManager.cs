5b0e6b2d6e38edf438136da5f84715bb83bb8229
Felipe Vasquez <minelyne@gmail.com>
2023-09-26 19:11:22 -0300

    more cinemachine cameras

---
+++ Assets/Scripts/CameraScripts/CameraManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Serialization;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private List<CinemachineVirtualCameraBase> _virtualCameras;
+    [SerializeField] private CinemachineVirtualCameraBase _defaultCamera;
+    private CinemachineVirtualCameraBase _activeCamera;
+
+    public void Start()
+    {
+        SwitchCamera(_defaultCamera);
+    }
+
+    public void SwitchCamera(CinemachineVirtualCameraBase camera)
+    {
+        foreach (CinemachineVirtualCameraBase virtualCameraBase in _virtualCameras)
+        {
+            virtualCameraBase.gameObject.SetActive(false);
+        }
+
+        camera.gameObject.SetActive(true);
+    }
+}
