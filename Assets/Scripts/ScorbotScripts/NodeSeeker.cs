e10012884e83cd8014e0430a518b16e293ec282f
Felipe Vasquez <minelyne@gmail.com>
2023-10-07 18:18:11 -0300

    NodeSeeker implementado con dijkstra

--- Assets/Scripts/ScorbotScripts/NodeSeeker.cs
+++ Assets/Scripts/ScorbotScripts/NodeSeeker.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using DG.Tweening;
 using UnityEngine;
 
 public class NodeSeeker : MonoBehaviour
@@ -9,6 +10,7 @@ public class NodeSeeker : MonoBehaviour
     [SerializeField] private Transform[] nodes;
     [SerializeField] private Transform origin, destination;
     private List<Node> connectedNodes = new List<Node>();
+
 
 
     public void Start()
@@ -47,25 +49,109 @@ public class NodeSeeker : MonoBehaviour
         }
     }
 
-    public void InvokeShortestPath()
-    {
-        Debug.Log(GetClosestNode(origin));
-    }
-
     private Transform GetClosestNode(Transform pos)
     {
         float closestDistance = float.MaxValue;
         Transform closestNode = null;
         foreach (Node node in connectedNodes)
         {
-            float distance = Vector3.Distance(pos.position,node.nodeA.position);
+            float distance = Vector3.Distance(pos.position, node.nodeA.position);
             if (distance < closestDistance)
             {
                 closestDistance = distance;
                 closestNode = node.nodeA.transform;
             }
         }
-    
+
         return closestNode;
+    }
+
+    public List<Transform> GenerateShortestPath()
+    {
+        Transform closestOriginNode = GetClosestNode(origin);
+        Transform closestDestinationNode = GetClosestNode(destination);
+
+        Dictionary<Transform, Node> nodeToParentMap = Dijkstra(closestOriginNode);
+        List<Transform> shortestPath = ReconstructPath(nodeToParentMap, closestDestinationNode);
+
+        //Si ya esta en la posicion destino, no agregar nodo cercano al origen a la lista
+        if (origin.position != destination.position)
+        {
+            shortestPath.Insert(0, GetClosestNode(origin));
+        }
+        //Añadir posicion destino
+        shortestPath.Add(destination);
+        
+        GetHeightSteps(shortestPath);
+        return shortestPath;
+    }
+
+    //Obtener pasos de altura entre el origen y destino 
+    public float GetHeightSteps(List<Transform> animationList)
+    { 
+        return (destination.position.y - origin.position.y) / (animationList.Count+2f);
+    }
+
+    //Algoritmo Dijkstra
+    private Dictionary<Transform, Node> Dijkstra(Transform startNode)
+    {
+        Dictionary<Transform, float> distance = new Dictionary<Transform, float>();
+        Dictionary<Transform, Node> parent = new Dictionary<Transform, Node>();
+        HashSet<Transform> visited = new HashSet<Transform>();
+
+        foreach (Transform node in nodes)
+        {
+            distance[node] = float.MaxValue;
+        }
+
+        distance[startNode] = 0;
+
+        PriorityQueue<Transform> priorityQueue = new PriorityQueue<Transform>();
+        priorityQueue.Enqueue(startNode, 0);
+
+        while (priorityQueue.count > 0)
+        {
+            Transform currentNode = priorityQueue.Dequeue();
+
+            if (visited.Contains(currentNode))
+                continue;
+
+            visited.Add(currentNode);
+            
+            //Iterar en nodos vecinos
+            foreach (Node neighborNode in connectedNodes)
+            {
+                if (neighborNode.nodeA == currentNode)
+                {
+                    Transform neighbor = neighborNode.nodeB;
+
+                    float newDistance = distance[currentNode] + neighborNode.weight;
+                    // Actualizar la distancia y el nodo padre si se un camino más corto
+                    if (newDistance < distance[neighbor])
+                    {
+                        distance[neighbor] = newDistance;
+                        parent[neighbor] = neighborNode;
+                        priorityQueue.Enqueue(neighbor, newDistance);
+                    }
+                }
+            }
+        }
+
+        return parent;
+    }
+
+    private List<Transform> ReconstructPath(Dictionary<Transform, Node> parentMap, Transform destinationNode)
+    {
+        List<Transform> path = new List<Transform>();
+        Transform current = destinationNode;
+
+        while (parentMap.ContainsKey(current))
+        {
+            path.Add(current);
+            current = parentMap[current].nodeA;
+        }
+
+        path.Reverse();
+        return path;
     }
 }
--- Assets/Scripts/ScorbotScripts/Tweener.cs
+++ Assets/Scripts/ScorbotScripts/Tweener.cs
@@ -6,11 +6,58 @@ using UnityEngine;
 
 public class Tweener : MonoBehaviour
 {
-    [SerializeField]private Transform destino;
-    
+    [SerializeField] private NodeSeeker nodeSeeker;
+    private Sequence tweenSequence;
+    [SerializeField] private float tweenSpeed;
+    private float offsetDistance;
 
-    private void Start()
+    public void TweenBetweenNodes()
     {
-        transform.DOMove(destino.position, 5, false).SetEase(Ease.InOutCubic).SetLoops(-1,LoopType.Yoyo);
+        List<Transform> shortestPath = nodeSeeker.GenerateShortestPath();
+        float step = nodeSeeker.GetHeightSteps(shortestPath);
+        
+        //Inicializacion de secuencia DOTween
+        tweenSequence = null;
+        tweenSequence = DOTween.Sequence();
+        //Curva de animacion para el primer punto
+        offsetDistance = 0;
+        Vector3 offsetPosition = AddOffset(shortestPath[0], step);
+        tweenSequence.Append(transform.DOMove(offsetPosition, tweenSpeed).SetEase(Ease.InSine));
+        //tweenSequence.Append(transform.DOMove(shortestPath[0].position, tweenSpeed).SetEase(Ease.InSine));
+
+        for (int i = 1; i < shortestPath.Count - 1; i++)
+        {
+            offsetPosition = AddOffset(shortestPath[i], step);
+            var path = transform.DOMove(offsetPosition, tweenSpeed).SetEase(Ease.Linear);
+            tweenSequence.Append(path);
+        }
+
+        //Curva de animacion para el ultimo punto
+        tweenSequence.Append(transform.DOMove(shortestPath[^1].position, tweenSpeed).SetEase(Ease.OutSine));
+        // offsetPosition = AddOffset(shortestPath[^1], step);
+        // tweenSequence.Append(transform.DOMove(offsetPosition, tweenSpeed).SetEase(Ease.OutSine));
+        
+    }
+
+    //Offset para la animacion de traslado
+    private Vector3 AddOffset(Transform node, float step)
+    {
+        Vector3 offsetPosition = node.position;
+        offsetPosition.y += offsetDistance;
+        offsetDistance += step;
+        return offsetPosition;
+    }
+
+    //Iniciar secuencia de animaciones
+    public void PlaySequence()
+    {
+        TweenBetweenNodes();
+        tweenSequence.Play();
+    }
+
+    //Cancelar secuencia de animaciones
+    public void StopSequence()
+    {
+        tweenSequence.Kill();
     }
 }
