124f699a51dcbbecdd601135afa8ad5cb63bd516
Alejandro Torres Jofré <ale.torres.jofre@gmail.com>
2023-10-25 12:06:43 -0300

    Añade string para cambiar velocidad desde la botonera

---
+++ Assets/Scripts/CanvasScripts/SpeedValue.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class SpeedValue : MonoBehaviour
+{
+    [SerializeField] private Text speedText;
+    
+    private string currentText = "_";
+    private string previousText;
+    
+    [SerializeField] private List<Button> interactableButtons = new List<Button>();
+    [SerializeField] private List<GameObject> numericButtons = new List<GameObject>();
+    [SerializeField] private List<GameObject> hideButtons = new List<GameObject>();
+
+
+    public void SpeedModifier()
+    {
+        previousText = speedText.text;
+        
+        foreach (Button buttons  in interactableButtons)
+        {
+            buttons.interactable=false;
+        }
+        
+        foreach (GameObject hideButton in hideButtons)
+        {
+            hideButton.SetActive(false);
+        }
+        
+        foreach (GameObject numericButton in numericButtons)
+        {
+            numericButton.SetActive(true);
+        }
+        
+        speedText.text = currentText;
+    }
+
+    public void ChangeSpeed(string number)
+    {
+        if (speedText.text == "_")
+        {
+            speedText.text = number;
+        }
+        else
+        {
+            speedText.text = speedText.text + number;
+        }
+        
+    }
+
+    public void Cancel()
+    {
+        speedText.text = previousText;
+        foreach (Button buttons  in interactableButtons)
+        {
+            buttons.interactable=true;
+        }
+        
+        foreach (GameObject hideButton in hideButtons)
+        {
+            hideButton.SetActive(true);
+        }
+        
+        foreach (GameObject numericButton in numericButtons)
+        {
+            numericButton.SetActive(false);
+        }
+    }
+
+    public void Confirm()
+    {
+        foreach (Button buttons  in interactableButtons)
+        {
+            buttons.interactable=true;
+        }
+        
+        foreach (GameObject hideButton in hideButtons)
+        {
+            hideButton.SetActive(true);
+        }
+        
+        foreach (GameObject numericButton in numericButtons)
+        {
+            numericButton.SetActive(false);
+        }
+    }
+}
