145907f14e5b9d307886a27f79c56365a42fde82
Felipe Vasquez <minelyne@gmail.com>
2023-11-04 16:21:04 -0300

    Collision reasignment & refactor collision manager

--- Assets/Scripts/ScorbotScripts/CollisionManager.cs
+++ Assets/Scripts/ScorbotScripts/CollisionManager.cs
@@ -6,23 +6,11 @@ namespace ScorbotScripts
     public class CollisionManager : MonoBehaviour
     {
         [SerializeField] private MeshCollider[] meshColliders;
-        private Material _highlightMaterial;
-        private bool highlightCollision;
-        private Material _baseMaterial;
-        private MeshRenderer _meshRenderer;
-        private CollisionBase _collisionBase;
+        [SerializeField] private int collisionLayer,defaultLayer;
 
 
         private void Start()
         {
-            //Obtener material de la pieza
-            _meshRenderer = GetComponent<MeshRenderer>();
-            _baseMaterial = _meshRenderer.material;
-
-            //Obtener material para destacar
-            _collisionBase = GetComponentInParent<CollisionBase>();
-            _highlightMaterial = _collisionBase.highlightMaterial;
-
             //Ignorar colisiones con piezas adyacentes
             MeshCollider modelMeshCollider = GetComponent<MeshCollider>();
             foreach (MeshCollider meshCollider in meshColliders)
@@ -34,14 +22,12 @@ namespace ScorbotScripts
         private void OnTriggerEnter(Collider other)
         {
             Debug.Log(gameObject.name + " esta chocando con " + other.name);
-            if (_collisionBase.highlightCollision) _meshRenderer.material = _highlightMaterial;
         }
 
 
         private void OnTriggerExit(Collider other)
         {
             Debug.Log(gameObject.name + " dejo de chocar");
-            if (_collisionBase.highlightCollision) _meshRenderer.material = _baseMaterial;
         }
     }
 }
