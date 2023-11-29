14ee68079476bb44139324a6edfc3f882d5c5e03
Alejandro Camilo Andre Torres Jofré <96086704+Relix47@users.noreply.github.com>
2023-11-29 18:37:58 -0300

    Actualiza materiales para la escena principal del usuario

---
+++ Assets/Scripts/MainMenuScript/CameraMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform point1;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
