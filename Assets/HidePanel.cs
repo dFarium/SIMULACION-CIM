cb32fd5494b342552d3a33eb84a7b9135e7b8e00
Felipe Vasquez <minelyne@gmail.com>
2023-11-30 18:18:47 -0300

    small fix

--- Assets/HidePanel.cs
+++ Assets/HidePanel.cs
@@ -16,13 +16,13 @@ public class HidePanel : MonoBehaviour
         if (isShowing)
         {
             panel.transform.DOMove(hidePanel.position, 1f).SetEase(Ease.InOutCubic);
-            image.transform.DORotate(new Vector3(0,180,0), 1f).SetEase(Ease.InOutCubic);
+            image.transform.DORotate(new Vector3(0,0,0), 1f).SetEase(Ease.InOutCubic);
             isShowing = false;
         }
         else
         {
             panel.transform.DOMove(showPanel.position, 1f).SetEase(Ease.InOutCubic);
-            image.transform.DORotate(new Vector3(0,0,0), 1f).SetEase(Ease.InOutCubic);
+            image.transform.DORotate(new Vector3(0,180,0), 1f).SetEase(Ease.InOutCubic);
             isShowing = true;
         }
     }
