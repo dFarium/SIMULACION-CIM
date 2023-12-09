58cee376aa0128059751e60c9bf74407d7bdd58b
Felipe Vasquez <minelyne@gmail.com>
2023-12-09 17:29:00 -0300

    Updated UI Production System / Updated Lightning prod system

--- Assets/Scripts/ProductionSystemScripts/ToggleWindowCanvasGroup.cs
+++ Assets/Scripts/ProductionSystemScripts/ToggleWindowCanvasGroup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class ToggleWindowCanvasGroup : MonoBehaviour
@@ -7,15 +8,72 @@ public class ToggleWindowCanvasGroup : MonoBehaviour
     [SerializeField] private GameObject backgroundPanel;
     [SerializeField] private CanvasGroup addForm;
     [SerializeField] private CanvasGroup removeForm;
+    private bool isAddFormActive;
     private CanvasGroup canvasGroup;
-    
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    public void ToggleWindow()
+    public void OpenRemoveItem()
+    {
+        //Si esta activo, no desactivar, solo cambiar el contenido
+        if (canvasGroup.alpha == 1 && isAddFormActive)
+        {
+            SwitchWindowForm(false);
+        }
+        else
+        {
+            ToggleWindow();
+            SwitchWindowForm(false);
+        }
+    }
+
+    public void OpenAddItem()
+    {
+        if (canvasGroup.alpha == 1 && !isAddFormActive)
+        {
+            SwitchWindowForm(true);
+        }
+        else
+        {
+            ToggleWindow();
+            SwitchWindowForm(true);
+        }
+    }
+
+    public void SwitchWindowForm(bool isAddForm)
+    {
+        //Mostrar addform si es true, mostrar removeform si es false
+        if (isAddForm)
+        {
+            //Ocultar RemoveForm y mostrar AddForm
+            addForm.alpha = 1;
+            addForm.interactable = true;
+            addForm.blocksRaycasts = true;
+            isAddFormActive = true;
+
+            removeForm.alpha = 0;
+            removeForm.interactable = false;
+            removeForm.blocksRaycasts = false;
+        }
+        else
+        {
+            //Ocultar AddForm y mostrar RemoveForm
+            addForm.alpha = 0;
+            addForm.interactable = false;
+            addForm.blocksRaycasts = false;
+            isAddFormActive = false;
+
+            removeForm.alpha = 1;
+            removeForm.interactable = true;
+            removeForm.blocksRaycasts = true;
+        }
+    }
+
+    private void ToggleWindow()
     {
         if (canvasGroup.alpha == 1)
         {
@@ -29,30 +87,6 @@ public class ToggleWindowCanvasGroup : MonoBehaviour
             canvasGroup.alpha = 1;
             canvasGroup.interactable = true;
             canvasGroup.blocksRaycasts = true;
-        }
-    }
-
-    public void SwitchWindowForm()
-    {
-        if (addForm.alpha == 0)
-        {
-            addForm.alpha = 1;
-            addForm.interactable = true;
-            addForm.blocksRaycasts = true;
-            
-            removeForm.alpha = 0;
-            removeForm.interactable = false;
-            removeForm.blocksRaycasts = false;
-        }
-        else
-        {
-            addForm.alpha = 0;
-            addForm.interactable = false;
-            addForm.blocksRaycasts = false;
-            
-            removeForm.alpha = 1;
-            removeForm.interactable = true;
-            removeForm.blocksRaycasts = true;
         }
     }
 }
